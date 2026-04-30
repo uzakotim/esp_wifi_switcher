291091deee42348dbeb20f0e9cf4612ba30f41d4
uzakotim <timauzakov@gmail.com>
2026-04-30 14:38:48 +0500

    init app

---
+++ WiFiSwitcherESP/WiFiSwitcherESP/WiFiSwitcherESPApp.swift
@@ -0,0 +1,17 @@
+//
+//  WiFiSwitcherESPApp.swift
+//  WiFiSwitcherESP
+//
+//  Created by Timur Uzakov on 4/30/26.
+//
+
+import SwiftUI
+
+@main
+struct WiFiSwitcherESPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
